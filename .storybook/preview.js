c346cdbfea72b95c7f276ea6d4ee86d5458f7613
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2021-01-05 14:20:00 +0500

    issue fixed. Now going to make a new project for practice once again

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,17 @@
 // Configures Storybook to log the actions(onArchiveTask and onPinTask) in the UI.
+
+import {Provider} from "react-redux";
+import {store} from '../src/store/TaskSlice'
+
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
 
+export const decorators = [
+  (Story) => (
+    <Provider store= {store} >
+      <Story />
+    </Provider>
+  ),
+];
